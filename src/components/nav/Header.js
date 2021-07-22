5109cf73bd7f8a8904be08368a02e37a0f959d40
Cannon-Cloud <clarence@cannon.cloud>
2021-07-22 15:47:49 -0700

    Conditional Menu Rendering

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -10,7 +10,7 @@ import {
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import firebase from "firebase";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
@@ -18,6 +18,7 @@ const { SubMenu, Item } = Menu;
 const Header = () => {
   const [current, setCurrent] = useState("home");
   let dispatch = useDispatch();
+  let { user } = useSelector((state) => ({ ...state }));
   let history = useHistory();
 
   const handleClick = (event) => {
@@ -36,31 +37,43 @@ const Header = () => {
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Item key="home" icon={<CodeOutlined />}>
-        <Link to="/">Home</Link>
+        <Link to="/">Home </Link>
       </Item>
-      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Settings">
-        <Item key="setting:1">Dashboard</Item>
-        <Item key="setting:2">Login</Item>
-        <Item icon={<LogoutOutlined />} onClick={logout} key="logout">
-          Logout
-        </Item>
-      </SubMenu>
 
-      <Item key="regsiter" className="float-end" icon={<UserAddOutlined />}>
-        <Link className="float-end" to="/register">
-          Register
-        </Link>
-      </Item>
-      <Item
-        key="login"
-        icon={<LoginOutlined />}
-        className="float-end"
-        style={{ float: "right" }}
-      >
-        <Link className="float-end" to="/login">
-          Login
-        </Link>
-      </Item>
+      {!user && (
+        <Item key="regsiter" className="float-end" icon={<UserAddOutlined />}>
+          <Link className="float-end" to="/register">
+            Register
+          </Link>
+        </Item>
+      )}
+      {!user && (
+        <Item
+          key="login"
+          icon={<LoginOutlined />}
+          className="float-end"
+          style={{ float: "right" }}
+        >
+          <Link className="float-end" to="/login">
+            Login
+          </Link>
+        </Item>
+      )}
+
+      {user && (
+        <SubMenu
+          key="SubMenu"
+          icon={<SettingOutlined />}
+          title={user.email && user.email.split("@")[0]}
+          className="float-end"
+        >
+          <Item key="setting:1">Dashboard</Item>
+          <Item key="setting:2">Login</Item>
+          <Item icon={<LogoutOutlined />} onClick={logout} key="logout">
+            Logout
+          </Item>
+        </SubMenu>
+      )}
     </Menu>
   );
 };
